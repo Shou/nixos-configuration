83fb33a2f720e9103da93828c90ffac36b205d25
Benedict Aas <x+g@shou.io>
2020-02-01 23:21:04 +0000

    Add git

--- home.nix
+++ home.nix
@@ -7,65 +7,6 @@
     firefox-bin google-chrome
   ]);
 
-  dconf.settings = {
-    "org/gnome/desktop/wm/keybindings" = {
-      "switch-to-workspace-up" = ["<Super>k"];
-      "switch-to-workspace-down" = ["<Super>j"];
-      "switch-to-workspace-1" = ["<Super>1"];
-      "switch-to-workspace-2" = ["<Super>2"];
-      "switch-to-workspace-3" = ["<Super>3"];
-      "switch-to-workspace-4" = ["<Super>4"];
-      "switch-to-workspace-5" = ["<Super>5"];
-      "switch-to-workspace-6" = ["<Super>6"];
-      "switch-to-workspace-7" = ["<Super>7"];
-      "switch-to-workspace-8" = ["<Super>8"];
-      "switch-to-workspace-9" = ["<Super>9"];
-      "switch-to-workspace-10"  = ["<Super>0"];
-
-      "move-to-workspace-up" = ["<Super><Shift>k"];
-      "move-to-workspace-down" = ["<Super><Shift>j"];
-      "move-to-workspace-1" = ["<Super><Shift>1"];
-      "move-to-workspace-2" = ["<Super><Shift>2"];
-      "move-to-workspace-3" = ["<Super><Shift>3"];
-      "move-to-workspace-4" = ["<Super><Shift>4"];
-      "move-to-workspace-5" = ["<Super><Shift>5"];
-      "move-to-workspace-6" = ["<Super><Shift>6"];
-      "move-to-workspace-7" = ["<Super><Shift>7"];
-      "move-to-workspace-8" = ["<Super><Shift>8"];
-      "move-to-workspace-9" = ["<Super><Shift>9"];
-      "move-to-workspace-10" = ["<Super><Shift>0"];
-
-      "close" = ["<Super>c"];
-      "toggle-fullscreen" = ["<Super>f"];
-      "toggle-maximized" = ["<Super>t"];
-    };
-
-    "desktop/ibus/general".preload-engines = [ "mozc-jp" "xkb:us:altgr-intl:eng" ];
-
-    "org/gnome/mutter/keybindings" = {
-      "toggle-tiled-left" = ["<Super>comma"];
-      "toggle-tiled-right" = ["<Super>period"];
-    };
-
-    "org/gnome/shell/app-switcher" = {
-      "current-workspace-only" = true;
-    };
-
-    "org/gnome/desktop/interface" = {
-      "clock-show-seconds" = true;
-      "clock-show-date" = true;
-      "clock-show-weekday" = true;
-    };
-
-    "org/gnome/desktop/calendar".show-weekdate = true;
-
-    "org/gnome/desktop/wm/preferences".resize-with-right-button = true;
-
-    # There is an option under packages.gnome-terminal.showMenubar for
-    # this but it doesn't work????
-    "org/gnome/terminal/legacy".default-show-menubar = false;
-  };
-
   programs.fish = {
     enable = true;
 
@@ -76,6 +17,25 @@
   };
 
   programs.direnv.enableFishIntegration = true;
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+
+    settings = {
+      character.symbol = "➜";
+    };
+  };
+
+  programs.ssh = {
+    # We don't want to manage this because it's PRIVATE!!!
+    enable = false;
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
 
   programs.tmux = {
     enable = true;
@@ -240,13 +200,63 @@ syntax keyword jsBooleanFalse false conceal cchar=👎
     '';
   };
 
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
+  dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      "switch-to-workspace-up" = ["<Super>k"];
+      "switch-to-workspace-down" = ["<Super>j"];
+      "switch-to-workspace-1" = ["<Super>1"];
+      "switch-to-workspace-2" = ["<Super>2"];
+      "switch-to-workspace-3" = ["<Super>3"];
+      "switch-to-workspace-4" = ["<Super>4"];
+      "switch-to-workspace-5" = ["<Super>5"];
+      "switch-to-workspace-6" = ["<Super>6"];
+      "switch-to-workspace-7" = ["<Super>7"];
+      "switch-to-workspace-8" = ["<Super>8"];
+      "switch-to-workspace-9" = ["<Super>9"];
+      "switch-to-workspace-10"  = ["<Super>0"];
 
-    settings = {
-      character.symbol = "➜";
+      "move-to-workspace-up" = ["<Super><Shift>k"];
+      "move-to-workspace-down" = ["<Super><Shift>j"];
+      "move-to-workspace-1" = ["<Super><Shift>1"];
+      "move-to-workspace-2" = ["<Super><Shift>2"];
+      "move-to-workspace-3" = ["<Super><Shift>3"];
+      "move-to-workspace-4" = ["<Super><Shift>4"];
+      "move-to-workspace-5" = ["<Super><Shift>5"];
+      "move-to-workspace-6" = ["<Super><Shift>6"];
+      "move-to-workspace-7" = ["<Super><Shift>7"];
+      "move-to-workspace-8" = ["<Super><Shift>8"];
+      "move-to-workspace-9" = ["<Super><Shift>9"];
+      "move-to-workspace-10" = ["<Super><Shift>0"];
+
+      "close" = ["<Super>c"];
+      "toggle-fullscreen" = ["<Super>f"];
+      "toggle-maximized" = ["<Super>t"];
     };
+
+    "desktop/ibus/general".preload-engines = [ "mozc-jp" "xkb:us:altgr-intl:eng" ];
+
+    "org/gnome/mutter/keybindings" = {
+      "toggle-tiled-left" = ["<Super>comma"];
+      "toggle-tiled-right" = ["<Super>period"];
+    };
+
+    "org/gnome/shell/app-switcher" = {
+      "current-workspace-only" = true;
+    };
+
+    "org/gnome/desktop/interface" = {
+      "clock-show-seconds" = true;
+      "clock-show-date" = true;
+      "clock-show-weekday" = true;
+    };
+
+    "org/gnome/desktop/calendar".show-weekdate = true;
+
+    "org/gnome/desktop/wm/preferences".resize-with-right-button = true;
+
+    # There is an option under packages.gnome-terminal.showMenubar for
+    # this but it doesn't work????
+    "org/gnome/terminal/legacy".default-show-menubar = false;
   };
 
   home.stateVersion = "19.03";
